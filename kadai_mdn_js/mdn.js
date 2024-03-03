52afea50e24c1f270fb4f0b87c81ffd241a39b58
yanepiyo <156920158+yanepiyo@users.noreply.github.com>
2024-03-04 00:48:05 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const date = new Date();
+
+const [month, day, year] = [
+    date.getMonth() + 1,
+    date.getDate(),
+    date.getFullYear(),
+];
+
+console.log(year + '年' + month + '月' + day + '日');
